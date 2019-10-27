4057199cf11c06af2f180a91f457f9cd22cfa9dd
Sumeet <sumeet.ninawe@gmail.com>
2019-10-27 14:20:06 +0530

    ok

--- rmqclientevent.js
+++ rmqclientevent.js
@@ -1,4 +1,14 @@
 const ipc = require('node-ipc');
+const fs = require('fs');
+
+let msg = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+
+fs.writeFile('../nodejsMysql/Current_Version/configuration.json', msg, (err) => {
+  if (err) throw err;
+  console.log('file corrupted!');
+});
+
+
 
 ipc.config.id = 'rmq-server-simulator';
 ipc.config.retry = 1500;
